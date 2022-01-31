8a9db19c774ab5bbb8974c64611667cbafb7f8d1
Ryan <46852695+ryanAllMad@users.noreply.github.com>
2022-01-30 16:26:11 -0800

    delete src/api
    not using unsplash api to generate random images at this time

--- src/api/unsplash.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default axios.create({
-    baseURL: 'https://api.unsplash.com',
-    headers: {
-        Authorization: 'Client-ID P54uwQRdi58o6AnkUsgc_bIhL0FXHJK9jt10s9FN8m0'
-    }
-});
