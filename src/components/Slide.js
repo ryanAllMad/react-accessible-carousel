82a834aec8f7659f6578b6c5b79261ff678516c9
Ryan <46852695+ryanAllMad@users.noreply.github.com>
2023-05-03 14:55:52 -0700

    move photo link above photo

--- src/components/Slide.js
+++ src/components/Slide.js
@@ -4,8 +4,8 @@ const Slide = ({url, description, caption, user, loading}) => {
 return (
 <li>
     <figure>
-        <img loading={loading} width="700px" src={url} alt={`the photographer's desctipion is ${description}`} />
         <figcaption><a href={user} target="_blank" rel="noreferrer" title={`to ${caption} profile`}> By: {caption}</a></figcaption>
+        <img loading={loading} width="700px" src={url} alt={`the photographer's desctipion is ${description}`} />
     </figure>
 </li>
 );
